ceaecec78da799b7567b2d861460f3061a456830
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-10-09 14:53:20 +0200

    Add test coverage for negative numbers

--- checkIfOdd.test.js
+++ checkIfOdd.test.js
@@ -5,11 +5,15 @@ describe('checkIfOdd', () => {
     it('should return true for odd numbers', () => {
         expect(checkIfOdd(1)).toBe(true);
         expect(checkIfOdd(3)).toBe(true);
+        expect(checkIfOdd(-1)).toBe(true);
+        expect(checkIfOdd(-3)).toBe(true);
     });
 
     it('should return false for even numbers', () => {
         expect(checkIfOdd(2)).toBe(false);
         expect(checkIfOdd(4)).toBe(false);
+        expect(checkIfOdd(-2)).toBe(false);
+        expect(checkIfOdd(-4)).toBe(false);
     });
 
     it('should return false for zero', () => {
