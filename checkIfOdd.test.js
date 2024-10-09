e196ea86dacd5eb0e52e2b2ad6ffc05fefc38913
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-10-09 15:43:20 +0200

    Add test coverage for string values

--- checkIfOdd.test.js
+++ checkIfOdd.test.js
@@ -19,4 +19,23 @@ describe('checkIfOdd', () => {
     it('should return false for zero', () => {
         expect(checkIfOdd(0)).toBe(false);
     });
+
+    it('should work with strings', () => { 
+        expect(checkIfOdd('1')).toBe(true);
+        expect(checkIfOdd('3')).toBe(true);
+        expect(checkIfOdd('-1')).toBe(true);
+        expect(checkIfOdd('-3')).toBe(true);
+
+        expect(checkIfOdd('2')).toBe(false);
+        expect(checkIfOdd('4')).toBe(false);
+        expect(checkIfOdd('-2')).toBe(false);
+        expect(checkIfOdd('-4')).toBe(false);
+
+        expect(checkIfOdd('0')).toBe(false);
+    });
+
+    it('should return false for unexpected string values', () => { 
+        expect(checkIfOdd('')).toBe(false);
+        expect(checkIfOdd('Alphanumerical string')).toBe(false);
+    });
 });
